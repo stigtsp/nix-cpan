be56407a5a21b1b97892c0d606c2845beffab741
Stig Palmquist <stig@stig.io>
2020-12-13 02:39:54 +0100

    it's alive!
    * use Applify for cli options etc.
    * support --commit with some sanity checks

--- nix-update-perl-packages.pl
+++ nix-update-perl-packages.pl
@@ -1,35 +1,105 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=/home/sgo/nixpkgs/ -i perl -p perl perlPackages.RegexpCommon perlPackages.Mojolicious perlPackages.SmartComments perlPackages.MetaCPANClient perlPackages.SortVersions perlPackages.HTTPTinyCache perlPackages.TryTiny perlPackages.TextDiff
+#! nix-shell -i perl -p perl perlPackages.Applify perlPackages.RegexpCommon perlPackages.Mojolicious perlPackages.SmartComments perlPackages.MetaCPANClient perlPackages.SortVersions perlPackages.HTTPTinyCache perlPackages.TryTiny perlPackages.TextDiff
+
+#
+# Typical usage:
+#   git checkout -b nix-update-perl/$(date +%s`)
+#   nix-update-perl-packages.pl --nix-file pkgs/top-level/perl-packages.nix --commit
+#
 
 use strict;
-use Text::Diff;
 use v5.32;
-use experimental 'signatures';
+use Data::Dumper;
+use HTTP::Tiny::Cache;
+use Regexp::Common;
+use Text::Diff;
+use Smart::Comments;
 use Try::Tiny;
 use Sort::Versions;
 use Mojo::File;
-use Regexp::Common;
-use Data::Dumper;
-use HTTP::Tiny::Cache;
 use JSON::PP 'decode_json';
+use Applify;
+use experimental 'signatures';
 
-my $ua = new HTTP::Tiny::Cache ( agent => 'nix-update-perl-packages/0.01' );
+version 0.01;
 
-my $nix_file = $ARGV[0];
-die "no .nix file provided" unless -f $ARGV[0];
+option file    => "nix_file" => "Path to .nix file to update";
+option string  => "attr"     => "Name of single attr to update";
+option bool  => "commit"    => "Create one commit per update (implies --inplace)" => 0;
+option bool  => "inplace"   => "Modify .nix file inplace, without committing" => 0;
+option bool  => "verbose"   => "Show changes and determinations being made" => 0;
 
-my $nix = Mojo::File->new($nix_file)->slurp;
+
+our $ua = new HTTP::Tiny::Cache ( agent => 'nix-update-perl-packages/0.01' );
+
+app {
+    my $app = shift;
+
+    unless ($app->nix_file) {
+        $app->_script->print_help;
+        return 0;
+    }
+
+    my $nix_file = $app->nix_file;
+    die "no .nix file provided" unless -f $nix_file;
+    my $nix = Mojo::File->new($nix_file)->slurp;
 
 
-my (@modules, @skipped);
-while ($nix =~ m/(\w+)\s+=\s+buildPerl(Package|Module)\s+
-                 ($RE{balanced}{-parens=>'{}'})/gx) {
-    my ($attrname, $type, $part) = ($1, $2, $3);
-    push @modules, parse_module($attrname, $type, $part);
+
+    my (@modules);
+    while ($nix =~ m/((\w+)\s+=\s+buildPerl(Package|Module)\s+(?:rec)?\s*)
+                     ($RE{balanced}{-parens=>'{}'})/gx) {
+        my ($prepart, $attrname, $type, $part) = ($1, $2, $3, $4);
+        if (!$app->attr || $app->attr eq $attrname) {
+            push @modules, parse_module($attrname, $type, $part);
+        }
+    }
+
+    warn "Got ".scalar(@modules)." releases to update";
+
+    if ($app->commit) {
+        git_sanity($nix_file);
+    }
+
+    for my $module (@modules) {
+        die "Woops? No new or old code" unless $module->{old_code} && $module->{new_code};
+        if ($app->inplace || $app->commit) {
+            #git_sanity($nix_file) if ($app->commit); # Maybe a bit excessive to
+                                                      # check this before every
+                                                      # commit
+            my $newnix = Mojo::File->new($nix_file)->slurp;
+            $newnix =~ s/\Q$module->{old_code}\E/$module->{new_code}/gs;
+            Mojo::File->new($nix_file)->spurt($newnix);
+            if ($app->commit) {
+                git_commit($nix_file, "perlPackages.$module->{attrname}: $module->{old_version} -> $module->{new_version}");
+            }
+        }
+
+    }
+};
+
+sub git_sanity ($nix_file) {
+    my @err;
+    chomp(my $pwd_branch = qx(git rev-parse --abbrev-ref HEAD));
+    push @err, "* pwd is in '$pwd_branch' branch, this is probably not what you want\n"
+      if $pwd_branch=~/(master|main)/;
+
+    chomp(my $file_status = qx(git status --porcelain $nix_file));
+    push @err, "* $nix_file is not clean, maybe it has uncommitted changes?\n"
+      if $file_status || $?;
+
+    die join("", @err) if @err;
+}
+
+sub git_commit ($nix_file, $message) {
+    my @git_cmd = (qw(git commit --no-gpg-sign -m), $message, $nix_file);
+    (system(@git_cmd) == 0) or die "commit failed";
 }
 
 sub parse_module ($attrname, $type, $part) {
     my $m = {};
+
+    $m->{attrname} = $attrname;
 
     $m->{url}     = get_attr($part, 'url');
     $m->{pname}   = get_attr($part, 'pname');
@@ -38,11 +108,11 @@ sub parse_module ($attrname, $type, $part) {
 
     unless ($m->{url} =~ m|^mirror://cpan/|) {
         warn "$attrname \$url is not mirror://cpan skipping";
-        next;
+        return;
     }
     unless ($m->{pname} && $m->{url} && $m->{version} && $m->{sha256}) {
         warn "$attrname missing \$version, \$pname, \$url, or \$sha256";
-        next;
+        return;
     }
 
     my $cpan = {};
@@ -58,16 +128,18 @@ sub parse_module ($attrname, $type, $part) {
 
         my $code = set_attrs($part,
                              version => $cpan->{version},
-                             url => $cpan->{download_url},
-                             sha256 => sha256_to_base32($cpan->{checksum_sha256})
+                             url     => $cpan->{download_url},
+                             sha256  => sha256_to_base32($cpan->{checksum_sha256})
                          );
 
+        $m->{old_version} = $m->{version};
+        $m->{new_version} = $cpan->{version};
         $m->{old_code} = $part;
         $m->{new_code} = $code;
+#        print(diff \$part, \$code);
 
-        print(diff \$part, \$code);
 
-        printf("%-30s | %8s | %8s \n", $m->{pname}, $m->{version}, $cpan->{version});
+        #printf("%-30s | %8s | %8s \n", $m->{pname}, $m->{version}, $cpan->{version});
     } else {
         return;
     }
@@ -80,10 +152,11 @@ sub get_release ($release) { # MetaCPAN::Client doesn't want to return
     my $url = "https://fastapi.metacpan.org/v1/release/$release";
     my $res = $ua->get($url);
     die "$res->{status} $res->{reason}: $url" unless $res->{success};
-    my $release = decode_json($res->{content});
-    $release->{version} =~ s/^v//;
-    $release->{download_url} =~ s|^https://cpan.metacpan.org/|mirror://cpan/|;
-    return $release;
+
+    my $ret = decode_json($res->{content});
+    $ret->{version} =~ s/^v//;
+    $ret->{download_url} =~ s|^https://cpan.metacpan.org/|mirror://cpan/|;
+    return $ret;
 }
 
 sub get_download_url ($module) {
