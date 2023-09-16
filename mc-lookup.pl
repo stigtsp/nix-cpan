b423484e37b31d8d466847ad4217260a96bdf4d1
Stig Palmquist <git@stig.io>
2023-09-16 23:03:53 +0200

    mc-lookup: accept distro as arg

--- mc-lookup.pl
+++ mc-lookup.pl
@@ -5,6 +5,8 @@ use lib qw(lib);
 use Nix::MetaCPANCache;
 use Cpanel::JSON::XS qw(encode_json);
 
+my $distro = $ARGV[0] || die "$0 <distribution>\n";
+
 
 my $mc = Nix::MetaCPANCache->new();
-say encode_json( $mc->get_by_distribution("Moose")->data );
+say encode_json( $mc->get_by_distribution($distro)->data );
